ab408d3ac3334b345b1a1abd8999bb62599b04f1
Elijah Samuels <elijahsamuels@gmail.com>
2021-06-24 13:17:34 -0600

    added create react app

---
+++ pet-feeder/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Food from './components/foodData';
+import Water from './components/waterData';
+
+function App() {
+  return (
+    <div className="App">
+<Food />
+<Water />
+    </div>
+  );
+}
+
+export default App;
